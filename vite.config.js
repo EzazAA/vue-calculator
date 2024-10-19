c638ad51b5b9e3ee6ca07bea51a778758226986f
Ezaz Alam Ahmed <157497982+EzazAA@users.noreply.github.com>
2024-10-19 18:07:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Add the base option here
+  base: '/vue-calculator/',  // <-- Set this to match your GitHub repo name
   plugins: [
     vue(),
   ],
